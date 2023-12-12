847000c32dd23654aa32bdcbfbb572af479ac325
PriyanshuLodha <66645700+PriyanshuLodha@users.noreply.github.com>
2023-12-12 14:46:41 +0530

    Add files via upload

---
+++ codecrafter-api-gateway/src/test/java/com/example/codecrafterapigateway/CodecrafterApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codecrafterapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodecrafterApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
